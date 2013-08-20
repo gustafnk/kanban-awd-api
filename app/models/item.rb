ef0a6bebb7590ea6a65d90b9b9f90df049f9ca56
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-08-20 21:58:56 +0200

    Extract state transitions

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ActiveRecord::Base
+  def next_states
+    case self.status
+    when 'backlog'
+      ['working']
+    when 'working'
+      ['backlog', 'verify']
+    when 'verify'
+      ['working', 'done']
+    when 'done'
+      ['working']
+    end
+  end
+
+  def primary_action?(state)
+    state == next_states.reverse[0]
+  end
 end
