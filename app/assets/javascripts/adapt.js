4de04b696198ab5dd48f05cbd0fb4d42ef1f93b1
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-08-28 18:26:07 +0200

    Using native matchMedia

--- app/assets/javascripts/adapt.js
+++ app/assets/javascripts/adapt.js
@@ -1,24 +1,38 @@
 
-function loadBoard() {
-  var url = $("a[rel=board]").attr("href");
-  var column = $(".main.column");
-
-  // Avoid duplication, makes action idempotent
-  if($(".extra").length === 0) {
-    column.before($("<div class='extra'/>"));
-  }
-
-  $(".extra").load(url + " .columns");
-}
-
-$(document).on("onNavigationLoaded", loadBoard);
-
-$(document).on("click", ".main.column .actions input[type=submit]", function(e){
-  e.preventDefault();
-  var form = $(e.target).parent("form");
-
-  $.post(form.attr("action"), form.serialize(), function(response){
-    Turbolinks.visit(document.location.href)
-  });
+var mql = window.matchMedia("(min-width: 1024px)");
+mql.addListener(function(){
+  adapt();
 });
 
+var adapt = function(){
+
+  if (mql.matches) {
+    function loadBoard() {
+      var url = $("a[rel=board]").attr("href");
+      var column = $(".main.column");
+
+      // Avoid duplication, makes action idempotent
+      if($(".extra").length === 0) {
+        column.before($("<div class='extra'/>"));
+      }
+
+      $(".extra").load(url + " .columns");
+    }
+
+    loadBoard();
+
+    $(document).on("click", ".main.column .actions input[type=submit]", function(e){
+      e.preventDefault();
+      var form = $(e.target).parent("form");
+
+      $.post(form.attr("action"), form.serialize(), function(response){
+        Turbolinks.visit(document.location.href)
+      });
+    });
+  }
+  else {
+    $(".extra").empty();
+  }
+}
+
+$(document).on("onNavigationLoaded", adapt);
