2ad86a6be32ebc3015d5a72001049a063a6a9ef7
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-09-08 17:25:33 +0200

    Optimize classes for web or api

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,15 @@
 module ItemsHelper
+  def get_classes classes_string
+    if (not web?)
+      classes = classes_string.split(' ')
+      classes.select {|c| c.match('^[hp]-') }.join(' ')
+    else
+      classes_string
+    end
+  end
+
+  def web?
+    Rails::Server.new.options[:Port] != 3001 && !request.host.match("api")
+  end
+
 end
